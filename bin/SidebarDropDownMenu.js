b65897cc97e75bc98a277dbcb76cbf1de22db991
Michael <michael.danielczok@gmail.com>
2015-11-30 15:58:37 +0100

    Das Menü aus dem Basic Company Template wird als Control gebaut (noch nicht ganz fertig)

---
+++ bin/SidebarDropDownMenu.js
@@ -0,0 +1,132 @@
+/**
+ * Sidebar menu control
+ *
+ * @module package/quiqqer/menu/bin/js/SidebarDropDownMenu
+ * @author www.pcsg.de (Michael Danielczok, Henning Leutz)
+ *
+ * @require qui/QUI
+ * @require qui/controls/Control
+ * @require URL_OPT_DIR +bin/js/snap.svg-min.js
+ * @require css!package/quiqqer/template-qui/bin/js/ElasticMenu.css
+ */
+
+require.config({
+
+    baseUrl: URL_BIN_DIR + "QUI",
+
+    paths: {
+        "qui"     : URL_OPT_DIR + "bin/qui/qui",
+        "locale"  : URL_VAR_DIR + "locale/bin",
+        "controls": URL_BIN_DIR + "QUI/controls"
+    },
+
+    waitSeconds: 0,
+    catchError : true,
+
+    map: {
+        "*": {
+            "css": URL_OPT_DIR + "bin/qui/qui/lib/css.js"
+        }
+    }
+});
+
+window.addEvent("domready", function () {
+    "use strict";
+
+    // Load QUI
+    require(["qui/QUI"], function (QUI) {
+        QUI.addEvent("onError", function (msg, url, linenumber) {
+            console.error(msg);
+            console.error(url);
+            console.error("LineNo: " + linenumber);
+        });
+    });
+
+    /*
+     * Left nav:
+     * open or close the sub nav
+     */
+
+    var ToggleButton = document.getElements(".quiqqer-fa-levels-icon");
+
+    ToggleButton.addEvent("click", function () {
+        var LiLeft = this.getParent().getParent();
+        var NavSubLeft = LiLeft.getElement("div");
+
+        /*
+         * Wenn menü ZU ist:
+         */
+        if (!NavSubLeft.getSize().y.toInt()) {
+
+            NavSubLeft.setStyles({
+                height  : 0,
+                opacity : 0,
+                overflow: "hidden",
+                display : "block"
+            });
+
+            moofx(NavSubLeft).animate({
+                height  : NavSubLeft.getElement("ul").getSize().y.toInt(),
+                opacity : 1
+            }, {
+                duration: 250,
+                callback: function () {
+                    moofx(NavSubLeft).animate({
+                        height: "100%"
+                    });
+                }
+            });
+            this.addClass("fa-nav-levels-rotate");
+            return;
+        }
+
+        /*
+         * Wenn menü AUF ist:
+         */
+        NavSubLeft.setStyle("overflow", "hidden");
+
+        moofx(NavSubLeft).animate({
+            height : 0,
+            opacity: 0
+        }, {
+            duration: 250,
+            callback: function () {
+                moofx(NavSubLeft).animate({
+                    height: 0
+                });
+            }
+        });
+        this.removeClass("fa-nav-levels-rotate");
+    });
+
+
+    /*
+     * Mobile nav:
+     * open or close the sub nav
+     */
+    var Nav = document.id("navigation");
+    var NavButton = document.getElement("nav.nav-box");
+    var NavButtonDropDown = document.getElements(".fa-chevron-down");
+
+    NavButtonDropDown.addEvent("click", function () {
+        var Li = this.getParent();
+        var NavSub = Li.getElement(".page-header-navigation-sub");
+        if (NavSub.getStyle("height").toInt() < 1) {
+            NavSub.addClass("nav-toggle-sub");
+            this.addClass("fa-chevron-down-rotate-mobile");
+        } else {
+            NavSub.removeClass("nav-toggle-sub");
+            this.removeClass("fa-chevron-down-rotate-mobile");
+        }
+    });
+
+    NavButton.addEvent("click", function () {
+        if (Nav.getStyle("height").toInt() < 1) {
+            Nav.addClass("nav-toggle");
+        } else {
+            Nav.removeClass("nav-toggle");
+        }
+
+    });
+
+});
---
+++ src/QUI/Menu/SidebarDropDownMenu.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This file contains \QUI\Menu\SidebarDropDownMenu
+ */
+
+namespace QUI\Menu;
+
+use QUI;
+use QUI\Projects\Site\Utils;
+
+/**
+ * Class SidebarDropDownMenu
+ * Creates on Sidebar Drop Down Menu
+ *
+ * @package QUI\Menu
+ * @author www.pcsg.de (Michael Danielczok, Henning Leutz)
+ */
+class SidebarDropDownMenu extends QUI\Control
+{
+    /**
+     * @param array $attributes
+     */
+    public function __construct($attributes = array())
+    {
+        // defaults values
+        $this->setAttributes(array(
+            'startId' => 1, // id or site link
+            'homeLink' => false,
+            'levels' => false,
+            'homeIcon' => 'fa-home',
+            'listIcon' => 'fa-angle-right',
+            'levelIcon' => 'fa-angle-double-down'
+        ));
+
+        parent::__construct($attributes);
+
+        $this->addCSSFile(
+            dirname(__FILE__) . '/SidebarDropDownMenu.css'
+        );
+
+        $this->setAttribute('class', 'quiqqer-sidebar-dropdown-navigation grid-100');
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine = QUI::getTemplateManager()->getEngine();
+        $Project = $this->getProject();
+        $activeId = false;
+
+        // start
+        try {
+            $startId = $this->getAttribute('startId');
+
+            if (Utils::isSiteLink($startId)) {
+                $Site = Utils::getSiteByLink($startId);
+
+            } else {
+                $Site = $Project->get((int)$startId);
+            }
+
+        } catch (QUI\Exception $Exception) {
+            QUI\System\Log::addWarning($Exception->getMessage());
+
+            return '';
+        }
+
+        // active site
+        $ActiveSite = QUI::getRewrite()->getSite();
+
+        if ($ActiveSite && $ActiveSite->getProject() == $Project) {
+            $activeId = $ActiveSite->getId();
+        }
+
+        // settings
+        $levels = (int)$this->getAttribute('levels');
+
+        if ($levels <= 0 || $this->getAttribute('levels') === false) {
+            $levels = false;
+        }
+
+        $Engine->assign(array(
+            'this' => $this,
+            'Project' => $this->getProject(),
+            'Site' => $Site,
+            'homeLink' => $homeLink = $this->getAttribute('homeLink'),
+            'activeId' => $activeId,
+            'navTemplate' => dirname(__FILE__) . '/Navigation.html',
+            'levels' => $levels,
+            'Rewrite' => QUI::getRewrite(),
+            'homeIcon' => $homeIcon = $this->getAttribute('homeIcon'),
+            'listIcon' => $listIcon = $this->getAttribute('listIcon'),
+            'levelIcon' => $levelIcon = $this->getAttribute('levelIcon')
+        ));
+
+        $html = $Engine->fetch(dirname(__FILE__) . '/Navigation.html');
+        $html = '<nav>' . $html . '</nav>';
+
+        return $html;
+    }
+}
