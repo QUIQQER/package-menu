e4b6581cba8d8dd7bc713d24ae78732a3ceba837
Michael Danielczok <michael@pcsg.de>
2025-12-17 13:32:16 +0100

    fix: add option to control content alignment

--- src/QUI/Menu/Controls/Tabs.php
+++ src/QUI/Menu/Controls/Tabs.php
@@ -53,6 +53,7 @@ class Tabs extends QUI\Control
             'enableDragToScroll' => true,
 
             // tabs content
+            'contentPos' => 'center', // top, center, bottom
             'contentImgMinWidth' => 200,
             // number; do not use large values, recommended is between 100 and 800
             'contentImgMaxWidth' => 400,
@@ -162,6 +163,14 @@ class Tabs extends QUI\Control
         if ($this->getAttribute('navWrapText') === 'noWrap') {
             $navWrapText = 'navText__noWrap';
         }
+
+        $contentPos = match ($this->getAttribute('contentPos')) {
+            'top' => 'flex-start',
+            'bottom' => 'flex-end',
+            default => 'center',
+        };
+
+        $this->setCustomVariable('contentPos', $contentPos);
 
         $contentImgMaxWidth = $this->getAttribute('contentImgMaxWidth');
         $contentImgMinWidth = $this->getAttribute('contentImgMinWidth');
